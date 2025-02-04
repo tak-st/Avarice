d2b05befd72c067b342367578e677282825223a4
Takuya Shintani <p2180264@gmail.com>
2025-02-04 14:37:50 +0900

    fix NonPositionalUnits

--- Avarice/LuminaSheets.cs
+++ Avarice/LuminaSheets.cs
@@ -9,7 +9,7 @@ internal static class LuminaSheets
 
     internal static void Init()
     {
-        NonPositionalUnits = [];//TODO: fix Svc.Data.GetExcelSheet<BNpcBase>()!.Where(x => x.Unknown10).Select(x => x.RowId).ToArray();
+        NonPositionalUnits = Svc.Data.GetExcelSheet<BNpcBase>()!.Where(x => x.IsOmnidirectional).Select(x => x.RowId).ToArray();
     }
 
     public static bool HasPositional(this IGameObject obj)
